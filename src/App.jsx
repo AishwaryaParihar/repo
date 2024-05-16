913469788a76041126a4d65c3fdc12ff3bed2486
schhaya <schhaya144@gmail.com>
2024-05-16 10:37:56 +0530

    day 2 change

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,7 @@ let food = ['dal','roti','chawal','bread','sabji',]
    <ul  className="list-group">
     {food.map((item)=>  (<li key={item} className="list-group-item">{item}</li>) )}
   <h1>hello this is githfgffgfdfdfdub class</h1>
+  <h2>hello second day nchanges</h2>
 
 </ul>
 
